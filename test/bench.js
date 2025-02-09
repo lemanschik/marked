40936e0f4f2463e37fa008c03db8cfa490928723
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-02-09 10:52:03 +0100

    Tests do now use the lib version and bench the dist

--- test/bench.js
+++ test/bench.js
@@ -2,7 +2,7 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { htmlIsEqual, getTests } from '@markedjs/testutils';
 
-import { marked as cjsMarked } from '../lib/marked.cjs';
+import { marked as cjsMarked } from '../dist/marked.cjs';
 import { marked as esmMarked } from '../lib/marked.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
